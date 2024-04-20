1132a8bfa07893704113ca86d51b8bd4b663c808
Jhony Renan Cupriva dos Santos <jhony.renan777@gmail.com>
2024-04-20 10:46:53 -0300

    Rota de autenticação de usuário logado

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 class Console:
     def __init__(self, nome):
@@ -24,16 +24,21 @@ jogo3 = Jogo('Valorant', 'FPS', console3)
 lista_jogos = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
     return render_template('lista.html', titulo='Jogos', jogos=lista_jogos, consoles=lista_consoles)
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
     return render_template('novo.html', titulo='Novo Jogo', consoles=lista_consoles)
 
-@app.route('/criar', methods=['POST'])
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -46,5 +51,26 @@ def criar():
     else:
         print("Console não encontrado:", nome_console)
     return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo='Faça seu Login')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'qwe123@' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        # flash(session['usuario_logado'] + ' logado com sucesso!')
+        flash('Login realizado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout realizado com sucesso!')
+    return redirect('/login')
 
 app.run(debug=True)
